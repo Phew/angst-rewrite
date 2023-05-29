7efbe4227c6268dc1c54f59672289b54163a3b8a
charge <74613350+Phew@users.noreply.github.com>
2023-05-29 02:48:13 +0000

    cleaned up cleanup.py

--- plugins/cleanup.py
+++ plugins/cleanup.py
@@ -1,31 +1,20 @@
 import os
-import mss.tools
-import requests
-import zipfile
-import threading
 
 class CleanUp():
 
     def __init__(self):
         self.app_data = os.getenv("LOCALAPPDATA")
-	    
         self.cleanup()
 
     def cleanup(self):
-            """
-            Removes all trace
-            of angst by deleting
-            log files left by
-            angst.
-            """
-            angst_dir = os.path.join(self.app_data, "Angst")
-            for subdir, dirs, files in os.walk(angst_dir):
-                for file in files:
-                    filepath = f"{subdir}{os.sep}{file}"
-                    os.remove(filepath)
-
-            for subdir, dirs, files in os.walk(angst_dir):
-                for d in dirs:
-                    os.rmdir(f"{subdir}\\{d}")
-            os.rmdir(angst_dir)
-            os.remove(os.path.join(self.app_data, f'Angst-[{os.getlogin()}].zip'))
+        """
+        Removes all trace
+        of angst by deleting
+        log files left by
+        angst.
+        """
+        angst_dir = os.path.join(self.app_data, "Angst")
+        for file in os.listdir(angst_dir):
+            os.remove(os.path.join(angst_dir, file))
+        os.rmdir(angst_dir)
+        os.remove(os.path.join(self.app_data, f'Angst-[{os.getlogin()}].zip'))
